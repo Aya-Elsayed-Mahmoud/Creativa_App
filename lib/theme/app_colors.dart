f4e5cbe9f9f035fa8c5610acc19a32eaf938b939
Aya Elsayed Mahmoud <ayae32450@gmail.com>
2026-08-25 14:42:59 +0300

    chore: setup project structure and app colors

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF3F51B5);
+  static const Color background = Color(0xFFF5F6FA);
+  static const Color success = Color(0xFF4CAF50);
+  static const Color grey = Color(0xFF9E9E9E);
+}
